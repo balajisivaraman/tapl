6528673be4579a4805b8671648eec06d4b6fd621
Balaji Sivaraman <sivaraman.balaji@gmail.com>
2014-04-24 18:32:20 +0530

    Remove readLined deprecation warning by using StdIn.readLine()

--- src/main/scala/com/balajisivaraman/tapl/arith/App.scala
+++ src/main/scala/com/balajisivaraman/tapl/arith/App.scala
@@ -3,11 +3,12 @@ package com.balajisivaraman.tapl.arith
 import scala.util.{Failure, Success}
 import com.balajisivaraman.tapl.arith.terms.{Value, Term}
 import org.parboiled2.ParseError
+import scala.io.StdIn
 
 object App extends App {
 
   import Evaluator.eval
-  val input = readLine()
+  val input = StdIn.readLine()
   val parsedInput = new ArithParser(input).ArithInput.run()
   val result: Option[Value] = parsedInput match {
     case Success(t: Term) => eval(t)
