14ac6e7175513a1fa2ffcdb666230394e15e7c1e
Balaji Sivaraman <sivaraman.balaji@gmail.com>
2014-04-24 18:31:40 +0530

    Update project to Scala 2.11 means adding scala-reflect as dep.

--- project/Build.scala
+++ project/Build.scala
@@ -2,29 +2,41 @@ import sbt._
 import Keys._
 
 object BuildSettings {
-  val buildOrganization = "BalajiSivaraman"
-  val buildVersion = "0.1.0"
-  val buildScalaVersion = "2.10.4"
+  val Organization = "BalajiSivaraman"
+  val Version = "0.1.0"
+  val ScalaVersion = "2.11.0"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
-    organization := buildOrganization,
-    version := buildVersion,
-    scalaVersion := buildScalaVersion
+    organization := Organization,
+    version := Version,
+    scalaVersion := ScalaVersion
   )
 }
 
+object Resolvers {
+  val SonatypeSnapshots = "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  val SonatypeReleases = "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases"
+  val AllResolvers = Seq(SonatypeSnapshots, SonatypeSnapshots)
+}
+
 object Dependencies {
-  val parboiled2 = "org.parboiled" %% "parboiled" % "2.0-M2"
-  val allDeps = Seq(parboiled2)
+  val Parboiled2 = "org.parboiled" %% "parboiled" % "2.0.0-RC1"
+  val ScalaReflect = "org.scala-lang" % "scala-reflect" % BuildSettings.ScalaVersion
+
+  val Specs2 = "org.specs2" %% "specs2" % "2.3.11" % "test"
+
+  val TestDeps = Seq(Specs2)
+  val AllDeps = Seq(Parboiled2, ScalaReflect ) ++ TestDeps
 }
 
 object SprayAuthBuild extends Build {
   import BuildSettings._
   import Dependencies._
+  import Resolvers._
   
   lazy val SprayAuthProject = Project(
     "tapl-scala",
     file ("."),
-    settings = buildSettings ++ Seq(libraryDependencies := allDeps)
+    settings = buildSettings ++ Seq(resolvers := AllResolvers, libraryDependencies := AllDeps)
   )
 }
